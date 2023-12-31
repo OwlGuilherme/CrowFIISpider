391454e003b5e973b13266306190337ff902b668
Guilherme Santos <guilhermesantos.adv@protonmail.com>
2023-12-30 23:25:45 -0300

    Melhoradas funções que lidam com o banco de dados

--- crowFIISpider/crowFIISpider/utils/database.py
+++ crowFIISpider/crowFIISpider/utils/database.py
@@ -3,24 +3,27 @@ import sqlite3
 
 def criar_db():
     with sqlite3.connect('fiis.db') as conn:
-        c = conn.cursor()
-        c.execute('''
+        conn.execute('''
             CREATE TABLE IF NOT EXISTS fiis (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
-                tipo TEXT,
-                link TEXT,
                 titulo TEXT,
+                link TEXT,
+                tipo TEXT,
                 descricao TEXT
             )
         ''')
 
 def inserir_dados(fii_data):
-    with sqlite3.connect('fiis.db') as conn:
-        c = conn.cursor()
-        c.execute('''
-            INSERT INTO fiis (tipo, link, titulo, descricao)
-            VALUES (?, ?, ?, ?)
-        ''', (
-            fii_data['tipo'], fii_data['link'], fii_data['titulo'], fii_data['descricao']
-        ))
+    try:
+        with sqlite3.connect('fiis.db') as conn:
+            conn.execute('''
+                    INSERT INTO fiis (titulo, link, tipo, descricao)
+                    VALUES (?, ?, ?, ?)
+                ''', (
+                    fii_data['titulo'], fii_data['link'], fii_data['tipo'], fii_data['descricao']
+                ))
+            conn.commit()
+    except Exception as e:
+        print(f'Erro ao inserir dados no banco de dados: {e}')
+       
 
