2ead87ea7f6b92f40495cb6d3366fc4968180eb1
Guilherme Santos <guilhermesantos.adv@protonmail.com>
2023-12-30 22:49:48 -0300

    Criada lógica para coletar dados dos fundos iniciados com a letra "A"

--- fiis.py
+++ fiis.py
@@ -3,8 +3,21 @@ import scrapy
 
 class FiisSpider(scrapy.Spider):
     name = "fiis"
-    allowed_domains = ["fiis.com.br"]
-    start_urls = ["https://fiis.com.br/"]
+    start_urls = ["https://fiis.com.br/lista-de-fundos-imobiliarios/"]
 
     def parse(self, response):
-        pass
+        fiis_a_elements = response.css('.tickerFilter__results__box-A [data-element="content-list-ticker"]')
+
+        for fii_element in fiis_a_elements:
+            tipo = fii_element.css('.tickerBox__type::text').get()
+            link = fii_element.css('.tickerBox__link_ticker::attr(href)').get()
+            titulo = fii_element.css('.tickerBox__title::text').get()
+            descricao = fii_element.css('.tickerBox__desc::text').get()
+
+            yield {
+                'tipo': tipo,
+                'link' : link,
+                'titulo' : titulo,
+                'descricao' : descricao
+            }
+        
