b054c5b14500832ac5860a7d5589b2099e20c9d6
Guilherme Santos <guilhermesantos.adv@protonmail.com>
2023-12-31 16:55:33 -0300

    Criação do banco da tabela para receber os dados detalhados

--- crowFIISpider/crowFIISpider/utils/database.py
+++ crowFIISpider/crowFIISpider/utils/database.py
@@ -14,6 +14,41 @@ def criar_db():
             )
         ''')
 
+        conn.execute('''
+            CREATE TABLE IF NOT EXISTS detalhes_fiis (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                fii_id INTEGER,
+                ticker TEXT,
+                nome TEXT,
+                dividend_yield REAL,
+                ultimo_rendimento REAL,
+                patrimonio_liquido REAL,
+                pvp TEXT,
+                cotacao_atual REAL,
+                mudanca REAL,
+                min_52_seman REAL,
+                max_52_seman REAL,
+                variacao REAL,
+                valor_em_caixa REAL,
+                liquidez_media_diaria REAL,
+                valor_patrimonial_p_cota REAL,
+                num_cotistas REAL,
+                participacao_ifix REAL,
+                administrador TEXT,
+                cnpj_adm TEXT,
+                cnpj TEXT,
+                nome_pregao TEXT,
+                num_cotas REAL,
+                patrimonio REAL,
+                tipo_anbima TEXT,
+                segmen_anbima TEXT,
+                segmento TEXT,
+                tipo_gestao TEXT,
+                publico_alvo TEXT,
+                FOREIGN KEY(fii_id) REFERENCES fiis(id)
+            )
+        ''')
+
 def inserir_dados(fii_data):
     try:
         with sqlite3.connect('fiis.db') as conn:
