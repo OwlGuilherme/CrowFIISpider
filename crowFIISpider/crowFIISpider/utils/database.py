9b403e2406f13b826b904af3b448956b9d18e6f3
Guilherme Santos <guilhermesantos.adv@protonmail.com>
2023-12-31 13:34:02 -0300

    Modificada estrutura do banco de dados

--- crowFIISpider/crowFIISpider/utils/database.py
+++ crowFIISpider/crowFIISpider/utils/database.py
@@ -1,4 +1,5 @@
 import sqlite3
+import pandas as pd
 
 
 def criar_db():
@@ -6,10 +7,10 @@ def criar_db():
         conn.execute('''
             CREATE TABLE IF NOT EXISTS fiis (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
-                titulo TEXT,
+                ticker TEXT,
                 link TEXT,
                 tipo TEXT,
-                descricao TEXT
+                nome TEXT
             )
         ''')
 
@@ -17,13 +18,19 @@ def inserir_dados(fii_data):
     try:
         with sqlite3.connect('fiis.db') as conn:
             conn.execute('''
-                    INSERT INTO fiis (titulo, link, tipo, descricao)
+                    INSERT INTO fiis (ticker, link, tipo, nome)
                     VALUES (?, ?, ?, ?)
                 ''', (
-                    fii_data['titulo'], fii_data['link'], fii_data['tipo'], fii_data['descricao']
+                    fii_data['ticker'], fii_data['link'], fii_data['tipo'], fii_data['nome']
                 ))
             conn.commit()
     except Exception as e:
         print(f'Erro ao inserir dados no banco de dados: {e}')
        
 
+def carregar_links():
+    with sqlite3.connect('fiis.db') as conn:
+        query = 'SELECT id, link FROM fiis'
+        df = pd.read_sql_query(query, conn)
+
+        return df
