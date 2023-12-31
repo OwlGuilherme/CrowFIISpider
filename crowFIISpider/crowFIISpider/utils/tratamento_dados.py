5d967f42c39e4f33e579822e573f940361adbc63
Guilherme Santos <guilhermesantos.adv@protonmail.com>
2023-12-31 13:56:56 -0300

    Implementada lógica para tratar variação positiva e negatva

--- crowFIISpider/crowFIISpider/utils/tratamento_dados.py
+++ crowFIISpider/crowFIISpider/utils/tratamento_dados.py
@@ -15,7 +15,19 @@ def converte_valor(valor_monetario):
         if sufixo in valor_monetario:
             valor_numerico *= fator
             break
+
+    return int(valor_numerico)
     
 def substituir_virgula(numero_c_virgula):
     numero_c_ponto = ''.join(c if c.isdigit() or c == ',' else '.' for c in numero_c_virgula)
     return float(numero_c_ponto)
+
+def trata_variacao(variacao_element):
+    variacao_texto = variacao_element.xpath('.//text()').get()
+    variacao_limpa = ''.join(filter(lambda x: x.isdigit() or x == '.' or x == '%', variacao_texto))
+    variacao_numerica = float(variacao_limpa.replace(',', '.'))
+
+    if 'down' in variacao_element.xpath('./@class').get():
+        variacao_numerica *= -1
+
+    return variacao_numerica
