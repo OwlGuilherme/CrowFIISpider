ce1715c124185d98c55679ea21c272bd2b4ee2f9
Guilherme Santos <guilhermesantos.adv@protonmail.com>
2023-12-30 22:49:23 -0300

    Criada função para inserir dados no banco de dados

--- crowFIISpider/crowFIISpider/utils/database.py
+++ crowFIISpider/crowFIISpider/utils/database.py
@@ -4,9 +4,8 @@ import sqlite3
 def criar_db():
     with sqlite3.connect('fiis.db') as conn:
         c = conn.cursor()
-
         c.execute('''
-            CREATE TABLE IF NOT EXISTIR fiis (
+            CREATE TABLE IF NOT EXISTS fiis (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 tipo TEXT,
                 link TEXT,
@@ -14,3 +13,14 @@ def criar_db():
                 descricao TEXT
             )
         ''')
+
+def inserir_dados(fii_data):
+    with sqlite3.connect('fiis.db') as conn:
+        c = conn.cursor()
+        c.execute('''
+            INSERT INTO fiis (tipo, link, titulo, descricao)
+            VALUES (?, ?, ?, ?)
+        ''', (
+            fii_data['tipo'], fii_data['link'], fii_data['titulo'], fii_data['descricao']
+        ))
+
