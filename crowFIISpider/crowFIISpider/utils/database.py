c05510a43e73334c0f3ec387f3a756a52ae9ab1f
Guilherme Santos <guilhermesantos.adv@protonmail.com>
2024-01-04 21:42:34 -0300

    Adequada formatação há pep8

--- crowFIISpider/crowFIISpider/utils/database.py
+++ crowFIISpider/crowFIISpider/utils/database.py
@@ -1,12 +1,14 @@
+import json
 import sqlite3
+
 import pandas as pd
 import polars as pl
-import json
 
 
 def criar_db():
-    with sqlite3.connect('fiis.db') as conn:
-        conn.execute('''
+    with sqlite3.connect("fiis.db") as conn:
+        conn.execute(
+            """
             CREATE TABLE IF NOT EXISTS fiis (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 ticker TEXT,
@@ -14,9 +16,11 @@ def criar_db():
                 tipo TEXT,
                 nome TEXT
             )
-        ''')
+        """
+        )
 
-        conn.execute('''
+        conn.execute(
+            """
         CREATE TABLE IF NOT EXISTS detalhes_fiis (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             fii_id INTEGER,
@@ -50,25 +54,33 @@ def criar_db():
             dados_tabela_json TEXT,
             FOREIGN KEY(fii_id) REFERENCES fiis(id)
         )
-    ''')
+    """
+        )
+
 
 def inserir_dados(fii_data):
     try:
-        with sqlite3.connect('fiis.db') as conn:
-            conn.execute('''
+        with sqlite3.connect("fiis.db") as conn:
+            conn.execute(
+                """
                     INSERT INTO fiis (ticker, link, tipo, nome)
                     VALUES (?, ?, ?, ?)
-                ''', (
-                    fii_data['ticker'], fii_data['link'], fii_data['tipo'], fii_data['nome']
-                ))
+                """,
+                (
+                    fii_data["ticker"],
+                    fii_data["link"],
+                    fii_data["tipo"],
+                    fii_data["nome"],
+                ),
+            )
             conn.commit()
     except Exception as e:
-        print(f'Erro ao inserir dados no banco de dados: {e}')
-       
+        print(f"Erro ao inserir dados no banco de dados: {e}")
+
 
 def carregar_links():
-    with sqlite3.connect('fiis.db') as conn:
-        query = 'SELECT id, link FROM fiis'
+    with sqlite3.connect("fiis.db") as conn:
+        query = "SELECT id, link FROM fiis"
         df = pd.read_sql_query(query, conn)
 
         return df
@@ -76,20 +88,26 @@ def carregar_links():
 
 def inserir_dados_detalhados(detalhes_fii_data, dados_tabela_yield):
     try:
-        with sqlite3.connect('fiis.db') as conn:
+        with sqlite3.connect("fiis.db") as conn:
             # Verificar se os dados já existem no banco de dados
-            existing_data = conn.execute('''
+            existing_data = conn.execute(
+                """
                 SELECT id FROM detalhes_fiis
                 WHERE fii_id = ? AND ticker = ? AND nome = ?
-            ''', (
-                detalhes_fii_data['fii_id'], detalhes_fii_data['ticker'], detalhes_fii_data['nome']
-            )).fetchone()
+            """,
+                (
+                    detalhes_fii_data["fii_id"],
+                    detalhes_fii_data["ticker"],
+                    detalhes_fii_data["nome"],
+                ),
+            ).fetchone()
 
             dados_tabela_json = dados_tabela_yield
 
             if existing_data:
                 # Se os dados já existem, atualize-os em vez de inseri-los novamente
-                conn.execute('''
+                conn.execute(
+                    """
                     UPDATE detalhes_fiis
                     SET
                         fii_id = ?,
@@ -122,41 +140,44 @@ def inserir_dados_detalhados(detalhes_fii_data, dados_tabela_yield):
                         publico_alvo = ?,
                         dados_tabela_json = ?
                     WHERE id = ?
-                ''', (
-                    detalhes_fii_data['fii_id'],
-                    detalhes_fii_data['ticker'],
-                    detalhes_fii_data['nome'],
-                    detalhes_fii_data['dividend_yield'],
-                    detalhes_fii_data['ultimo_rendimento'],
-                    detalhes_fii_data['patrimonio_liquido'],
-                    detalhes_fii_data['P/VP'],
-                    detalhes_fii_data['cotacao_atual'],
-                    detalhes_fii_data['mudanca'],
-                    detalhes_fii_data['min_52_seman'],
-                    detalhes_fii_data['max_52_seman'],
-                    detalhes_fii_data['variacao'],
-                    detalhes_fii_data['valor_em_caixa'],
-                    detalhes_fii_data['liquidez_media_diaria'],
-                    detalhes_fii_data['valor_patrimonial_P_cota'],
-                    detalhes_fii_data['num_cotistas'],
-                    detalhes_fii_data['participacao_ifix'],
-                    detalhes_fii_data['administrador'],
-                    detalhes_fii_data['cnpj_adm'],
-                    detalhes_fii_data['cnpj'],
-                    detalhes_fii_data['nome_pregao'],
-                    detalhes_fii_data['num_cotas'],
-                    detalhes_fii_data['patrimonio'],
-                    detalhes_fii_data['tipo_anbima'],
-                    detalhes_fii_data['segmen_anbima'],
-                    detalhes_fii_data['segmento'],
-                    detalhes_fii_data['tipo_gestao'],
-                    detalhes_fii_data['publico_alvo'],
-                    dados_tabela_json,
-                    existing_data[0]
-                ))
+                """,
+                    (
+                        detalhes_fii_data["fii_id"],
+                        detalhes_fii_data["ticker"],
+                        detalhes_fii_data["nome"],
+                        detalhes_fii_data["dividend_yield"],
+                        detalhes_fii_data["ultimo_rendimento"],
+                        detalhes_fii_data["patrimonio_liquido"],
+                        detalhes_fii_data["P/VP"],
+                        detalhes_fii_data["cotacao_atual"],
+                        detalhes_fii_data["mudanca"],
+                        detalhes_fii_data["min_52_seman"],
+                        detalhes_fii_data["max_52_seman"],
+                        detalhes_fii_data["variacao"],
+                        detalhes_fii_data["valor_em_caixa"],
+                        detalhes_fii_data["liquidez_media_diaria"],
+                        detalhes_fii_data["valor_patrimonial_P_cota"],
+                        detalhes_fii_data["num_cotistas"],
+                        detalhes_fii_data["participacao_ifix"],
+                        detalhes_fii_data["administrador"],
+                        detalhes_fii_data["cnpj_adm"],
+                        detalhes_fii_data["cnpj"],
+                        detalhes_fii_data["nome_pregao"],
+                        detalhes_fii_data["num_cotas"],
+                        detalhes_fii_data["patrimonio"],
+                        detalhes_fii_data["tipo_anbima"],
+                        detalhes_fii_data["segmen_anbima"],
+                        detalhes_fii_data["segmento"],
+                        detalhes_fii_data["tipo_gestao"],
+                        detalhes_fii_data["publico_alvo"],
+                        dados_tabela_json,
+                        existing_data[0],
+                    ),
+                )
             else:
                 # Se os dados não existem, insira-os normalmente
-                conn.execute('''
+                conn.execute(
+                    """
                     INSERT INTO detalhes_fiis (
                         fii_id, ticker, nome, dividend_yield, ultimo_rendimento, patrimonio_liquido,
                         PVP, cotacao_atual, mudanca, min_52_seman, max_52_seman, variacao,
@@ -166,47 +187,50 @@ def inserir_dados_detalhados(detalhes_fii_data, dados_tabela_yield):
                         segmento, tipo_gestao, publico_alvo, dados_tabela_json
                     )
                     VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-                ''', (
-                    detalhes_fii_data['fii_id'],
-                    detalhes_fii_data['ticker'],
-                    detalhes_fii_data['nome'],
-                    detalhes_fii_data['dividend_yield'],
-                    detalhes_fii_data['ultimo_rendimento'],
-                    detalhes_fii_data['patrimonio_liquido'],
-                    detalhes_fii_data['P/VP'],
-                    detalhes_fii_data['cotacao_atual'],
-                    detalhes_fii_data['mudanca'],
-                    detalhes_fii_data['min_52_seman'],
-                    detalhes_fii_data['max_52_seman'],
-                    detalhes_fii_data['variacao'],
-                    detalhes_fii_data['valor_em_caixa'],
-                    detalhes_fii_data['liquidez_media_diaria'],
-                    detalhes_fii_data['valor_patrimonial_P_cota'],
-                    detalhes_fii_data['num_cotistas'],
-                    detalhes_fii_data['participacao_ifix'],
-                    detalhes_fii_data['administrador'],
-                    detalhes_fii_data['cnpj_adm'],
-                    detalhes_fii_data['cnpj'],
-                    detalhes_fii_data['nome_pregao'],
-                    detalhes_fii_data['num_cotas'],
-                    detalhes_fii_data['patrimonio'],
-                    detalhes_fii_data['tipo_anbima'],
-                    detalhes_fii_data['segmen_anbima'],
-                    detalhes_fii_data['segmento'],
-                    detalhes_fii_data['tipo_gestao'],
-                    detalhes_fii_data['publico_alvo'],
-                    dados_tabela_json
-                ))
+                """,
+                    (
+                        detalhes_fii_data["fii_id"],
+                        detalhes_fii_data["ticker"],
+                        detalhes_fii_data["nome"],
+                        detalhes_fii_data["dividend_yield"],
+                        detalhes_fii_data["ultimo_rendimento"],
+                        detalhes_fii_data["patrimonio_liquido"],
+                        detalhes_fii_data["P/VP"],
+                        detalhes_fii_data["cotacao_atual"],
+                        detalhes_fii_data["mudanca"],
+                        detalhes_fii_data["min_52_seman"],
+                        detalhes_fii_data["max_52_seman"],
+                        detalhes_fii_data["variacao"],
+                        detalhes_fii_data["valor_em_caixa"],
+                        detalhes_fii_data["liquidez_media_diaria"],
+                        detalhes_fii_data["valor_patrimonial_P_cota"],
+                        detalhes_fii_data["num_cotistas"],
+                        detalhes_fii_data["participacao_ifix"],
+                        detalhes_fii_data["administrador"],
+                        detalhes_fii_data["cnpj_adm"],
+                        detalhes_fii_data["cnpj"],
+                        detalhes_fii_data["nome_pregao"],
+                        detalhes_fii_data["num_cotas"],
+                        detalhes_fii_data["patrimonio"],
+                        detalhes_fii_data["tipo_anbima"],
+                        detalhes_fii_data["segmen_anbima"],
+                        detalhes_fii_data["segmento"],
+                        detalhes_fii_data["tipo_gestao"],
+                        detalhes_fii_data["publico_alvo"],
+                        dados_tabela_json,
+                    ),
+                )
 
             conn.commit()
     except Exception as e:
-        print(f'Erro ao inserir dados detalhados no banco de dados: {e}')
+        print(f"Erro ao inserir dados detalhados no banco de dados: {e}")
 
 
 def spider_closed(self, spider, reason):
     # Fechar a conexão com o banco de dados quando a spider for fechada
-    if hasattr(self, 'conn') and self.conn is not None:
+    if hasattr(self, "conn") and self.conn is not None:
         self.conn.close()
+
 
 def extrair_dados_tabela_yield(self, response):
     dados = []
@@ -227,16 +251,15 @@ def extrair_dados_tabela_yield(self, response):
 
 
 def carregar_dividendos():
-
     ticker = input("Digite o ticker do FII desejado: ").upper()
 
     try:
-        with sqlite3.connect('fiis.db') as conn:
-            query = f'''
+        with sqlite3.connect("fiis.db") as conn:
+            query = f"""
                 SELECT dados_tabela_json
                 FROM detalhes_fiis
                 WHERE ticker = ?    
-            '''            
+            """
 
             result = conn.execute(query, (ticker,)).fetchone()
 
@@ -244,8 +267,8 @@ def carregar_dividendos():
 
             if result:
                 dados_tabela_json = result[0]
-                
-                json_data =  json.loads(dados_tabela_json)
+
+                json_data = json.loads(dados_tabela_json)
 
                 df = pl.DataFrame(json_data)
 
@@ -255,5 +278,3 @@ def carregar_dividendos():
 
     except Exception as e:
         print(f"Erro ao recuperar dados do banco de dados: {e}")
-
-        
--- crowFIISpider/crowFIISpider/utils/tratamento_dados.py
+++ crowFIISpider/crowFIISpider/utils/tratamento_dados.py
@@ -1,16 +1,19 @@
-import pandas as pd
-import numpy as np
 from datetime import datetime
+
+import pandas as pd
+
 
 def converte_valor(valor_monetario):
     # Mapeia os sufixos para os fatores multiplicativos
-    sufixos = {'M': 1e6, 'B': 1e9}
-    
+    sufixos = {"M": 1e6, "B": 1e9}
+
     # Filtra apenas os dígitos, ponto e sufixos
-    valor_limpo = ''.join(filter(lambda x: x.isdigit() or x == '.' or x in sufixos, valor_monetario))
-    
+    valor_limpo = "".join(
+        filter(lambda x: x.isdigit() or x == "." or x in sufixos, valor_monetario)
+    )
+
     # Converte para float
-    valor_numerico = float(valor_limpo.replace(',', '.'))
+    valor_numerico = float(valor_limpo.replace(",", "."))
 
     # Multiplica pelo fator apropriado se houver sufixo
     for sufixo, fator in sufixos.items():
@@ -19,33 +22,39 @@ def converte_valor(valor_monetario):
             break
 
     return int(valor_numerico)
-    
+
+
 def substituir_virgula(numero_c_virgula):
-    numero_c_ponto = ''.join(c if c.isdigit() or c == ',' else '.' for c in numero_c_virgula)
+    numero_c_ponto = "".join(
+        c if c.isdigit() or c == "," else "." for c in numero_c_virgula
+    )
     return float(numero_c_ponto)
 
-def trata_variacao(variacao_element):
-    variacao_texto = variacao_element.xpath('.//text()').get()
-    variacao_limpa = ''.join(filter(lambda x: x.isdigit() or x == '.' or x == '%', variacao_texto))
-    variacao_numerica = float(variacao_limpa.replace(',', '.'))
 
-    if 'down' in variacao_element.xpath('./@class').get():
+def trata_variacao(variacao_element):
+    variacao_texto = variacao_element.xpath(".//text()").get()
+    variacao_limpa = "".join(
+        filter(lambda x: x.isdigit() or x == "." or x == "%", variacao_texto)
+    )
+    variacao_numerica = float(variacao_limpa.replace(",", "."))
+
+    if "down" in variacao_element.xpath("./@class").get():
         variacao_numerica *= -1
 
     return variacao_numerica
 
-def extrair_variacao(variacao_element):
-    
-    variacao_texto = variacao_element.css('::text').get()
 
-    if 'up' in variacao_element.attrib['class']:
-        return float(variacao_texto.strip('%').replace(',', '.'))
-    elif 'down' in variacao_element.attrib['class']:
-        return -float(variacao_texto.strip('%').replace(',', '.'))
+def extrair_variacao(variacao_element):
+    variacao_texto = variacao_element.css("::text").get()
+
+    if "up" in variacao_element.attrib["class"]:
+        return float(variacao_texto.strip("%").replace(",", "."))
+    elif "down" in variacao_element.attrib["class"]:
+        return -float(variacao_texto.strip("%").replace(",", "."))
+
 
 def remover_ponto(numero_com_ponto):
-    return numero_com_ponto.replace('.', '')
-    
+    return numero_com_ponto.replace(".", "")
 
 
 def tratar_dados_tabela_yield(df):
@@ -53,50 +62,61 @@ def tratar_dados_tabela_yield(df):
         # Tratar as datas
         try:
             # Ajustar o formato da data (remover "\n" e converter para datetime)
-            data_base_str = row['Data Base']
+            data_base_str = row["Data Base"]
             if pd.notna(data_base_str):
-                df.at[i, 'Data Base'] = datetime.strptime(data_base_str.replace('\n', ''), '%d.%m.%Y').date().isoformat()
+                df.at[i, "Data Base"] = (
+                    datetime.strptime(data_base_str.replace("\n", ""), "%d.%m.%Y")
+                    .date()
+                    .isoformat()
+                )
             else:
-                df.at[i, 'Data Base'] = None
+                df.at[i, "Data Base"] = None
 
-            data_pagamento_str = row['Data Pagamento']
+            data_pagamento_str = row["Data Pagamento"]
             if pd.notna(data_pagamento_str):
-                df.at[i, 'Data Pagamento'] = datetime.strptime(data_pagamento_str.replace('\n', ''), '%d.%m.%Y').date().isoformat()
+                df.at[i, "Data Pagamento"] = (
+                    datetime.strptime(data_pagamento_str.replace("\n", ""), "%d.%m.%Y")
+                    .date()
+                    .isoformat()
+                )
             else:
-                df.at[i, 'Data Pagamento'] = None
+                df.at[i, "Data Pagamento"] = None
 
         except ValueError:
-            df.at[i, 'Data Base'] = None
-            df.at[i, 'Data Pagamento'] = None
+            df.at[i, "Data Base"] = None
+            df.at[i, "Data Pagamento"] = None
 
         # Tratar os valores numéricos
         try:
             # Ajustar o formato do número fracionário (substituir "," por "." e converter para float)
-            cotacao_base_str = row['Cotação Base']
+            cotacao_base_str = row["Cotação Base"]
             if pd.notna(cotacao_base_str):
-                df.at[i, 'Cotação Base'] = float(cotacao_base_str.replace('R$', '').replace(',', '.').strip())
+                df.at[i, "Cotação Base"] = float(
+                    cotacao_base_str.replace("R$", "").replace(",", ".").strip()
+                )
             else:
-                df.at[i, 'Cotação Base'] = None
+                df.at[i, "Cotação Base"] = None
 
-            dividend_yield_str = row['Dividend Yield']
+            dividend_yield_str = row["Dividend Yield"]
             if pd.notna(dividend_yield_str):
                 # Modificação para lidar com diferentes formatos de percentagem
-                df.at[i, 'Dividend Yield'] = float(dividend_yield_str.replace('%', '').replace(',', '.').strip())
+                df.at[i, "Dividend Yield"] = float(
+                    dividend_yield_str.replace("%", "").replace(",", ".").strip()
+                )
             else:
-                df.at[i, 'Dividend Yield'] = None
+                df.at[i, "Dividend Yield"] = None
 
             # Tratar o campo de rendimento
-            rendimento_str = row['Rendimento']
+            rendimento_str = row["Rendimento"]
             if pd.notna(rendimento_str):
-                df.at[i, 'Rendimento'] = float(rendimento_str.replace('R$', '').replace(',', '.').strip())
+                df.at[i, "Rendimento"] = float(
+                    rendimento_str.replace("R$", "").replace(",", ".").strip()
+                )
             else:
-                df.at[i, 'Rendimento'] = None
+                df.at[i, "Rendimento"] = None
         except ValueError:
-            df.at[i, 'Cotação Base'] = None
-            df.at[i, 'Dividend Yield'] = None
-            df.at[i, 'Rendimento'] = None
+            df.at[i, "Cotação Base"] = None
+            df.at[i, "Dividend Yield"] = None
+            df.at[i, "Rendimento"] = None
 
     return df
-
-    
-
